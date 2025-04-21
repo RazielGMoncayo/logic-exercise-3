e7d27568f593eb3d3520b9b798e9651094243e0c
RazielG <razielgmoncayo@gmail.com>
2025-04-21 16:33:21 -0600

    Factorial exercise finished

---
+++ script.js
@@ -0,0 +1,18 @@
+let contion = 0;
+
+do{
+    //! values user input 
+    let number = parseFloat(prompt("Enter the number to get the factorial: "));
+
+    if(!isNaN(number)){
+        let factorial = 1;
+        for(let i = 1; i <= number; i++){
+            factorial *= i;
+        }
+        console.log("The factorial of " + number + " is " + factorial);
+        contion = parseInt(prompt("Do you want to continue? (0 to exit, 1 to continue)"));
+    } else {
+        console.log("Enter only a number to get the factorial");
+    }
+
+} while(contion!=0);
