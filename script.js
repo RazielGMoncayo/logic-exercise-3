514b7e4935f1726b70fc0c4bc2a4fcd7d13be1af
RazielG <razielgmoncayo@gmail.com>
2025-04-21 16:43:37 -0600

    exercise and readme finished

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ do{
         for(let i = 1; i <= number; i++){
             factorial *= i;
         }
-        console.log("The factorial of " + number + " is " + factorial);
+        console.log("The factorial of " + number + " 5is " + factorial);
         contion = parseInt(prompt("Do you want to continue? (0 to exit, 1 to continue)"));
     } else {
         console.log("Enter only a number to get the factorial");
